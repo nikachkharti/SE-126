7aaa5717e4f1e6bf30ad45d7ffd0c5bf1b7948b8
Nikoloz Chkhartishvili <nika.chkhartishvili7@gmail.com>
2023-06-20 21:28:47 +0400

    Car model created

---
+++ SE-126/SE-126MainConsoleApp/Car.cs
@@ -0,0 +1,21 @@
+﻿namespace SE_126MainConsoleApp
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Cylinder { get; set; }
+        public float Engine { get; set; }
+        public string Drive { get; set; }
+        public string Transmission { get; set; }
+        public int City { get; set; }
+        public int Combined { get; set; }
+        public int Highway { get; set; }
+
+        public Car Parse(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
