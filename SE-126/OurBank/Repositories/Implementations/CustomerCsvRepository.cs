4bf44e8f9418046d881cd61c4ff2c6f1ebe9c080
beka-debug <bagaturia_b@itstep.org>
2023-07-11 22:05:35 +0400

    last commit

--- SE-126/OurBank/Repositories/Implementations/CustomerCsvRepository.cs
+++ SE-126/OurBank/Repositories/Implementations/CustomerCsvRepository.cs
@@ -6,6 +6,8 @@ namespace OurBank.Repositories
     {
         const string _filePath = @"../../../Data/Customers.csv";
         List<Customer> _customers = new();
+        int newElementAdded = 0;
+
         public CustomerCsvRepository()
         {
             _customers = File
@@ -17,15 +19,26 @@ namespace OurBank.Repositories
 
         public void Add(Customer model)
         {
-            //ჩამატბეამდე შეამოწმეთ მაქსიმალური ID და ახალი ჩანაწერისთვის გაზრდეთ  + 1 ით
-            _customers.Add(model);
-            //ახალი user დაიმახსოვრეთ ფაილში
+            if (!_customers.Any(x => x.Equals(model)))
+            {
+                //ჩამატბეამდე შეამოწმეთ მაქსიმალური ID და ახალი ჩანაწერისთვის გაზრდეთ  + 1 ით
+                model.Id = _customers.Max(x => x.Id) + 1;
+                _customers.Add(model);
+                //ახალი user დაიმახსოვრეთ ფაილში
+                SaveChanges(model);
+            }
+        }
+
+        private void SaveChanges(Customer customer)
+        {
+            File.AppendAllText(_filePath, customer.ToCsv());
         }
     }
 
     internal static class CustomerExtension
     {
         //ფაილში დამახსოვრებისთვის შექმენით კიდევ ერთი extension მეთოდი სახელად ToCsv()
+        internal static string ToCsv(this Customer obj) => $"\n{obj.Id},{obj.Name},{obj.IdentityNumber},{obj.PhoneNumber},{obj.Email},{obj.CustomerType}";
         internal static Customer ToCustomer(this string input)
         {
             Customer result = new();
