d9e70c424205152e53bc01e8fb1113feb24d8f1c
Admin <Admin@T-25-01>
2023-09-21 20:16:40 +0400

    Bsaic xaml project added

--- SE-126/Movie.App/MainWindow.xaml.cs
+++ SE-126/Movie.App/MainWindow.xaml.cs
@@ -1,7 +1,4 @@
-﻿using Movie.Models;
-using Movie.Service;
-using Movie.Service.Interfaces;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,50 +19,9 @@ namespace Movie.App
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly IUnitOfWork _unitOfWork;
-        private CountryModel countryToDelete;
-
         public MainWindow()
         {
             InitializeComponent();
-            _unitOfWork = new UnitOfWork();
-        }
-
-        private async void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                var allCountries = await _unitOfWork.Country.GetAllCountries();
-                allCountries.ForEach(country => CountryList.Items.Add(country));
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        private void CountryList_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            try
-            {
-                countryToDelete = CountryList.SelectedItem as CountryModel;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        private async void DeleteBtn_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                await _unitOfWork.Country.DeleteCountry(countryToDelete.CountryId);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
         }
     }
 }
