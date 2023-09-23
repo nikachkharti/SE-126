063b12652bb0a06de6178b77d1ab1fe233af34fe
Admin <Admin@T-25-01>
2023-09-23 22:00:25 +0400

    MVC project added

---
+++ SE-126/Movie.Web/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movie.Models;
+using Movie.Service;
+using Movie.Service.Interfaces;
+using System.Diagnostics;
+
+namespace Movie.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public HomeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CountryModel model)
+        {
+            await _unitOfWork.Country.AddCountry(model);
+            return RedirectToAction("Countries");
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var model = await _unitOfWork.Country.GetCountry(id);
+            return View(model);
+        }
+
+
+        public async Task<IActionResult> Countries()
+        {
+            List<CountryModel> allCountries = await _unitOfWork.Country.GetAllCountries();
+            return View(allCountries);
+        }
+    }
+}
